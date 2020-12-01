342c43f991c7d6f5104ae8b9318eeb9e8bfb85c9
Andrew Hummel <ahummel25@gmail.com>
2020-12-01 17:35:44 -0600

    Fix header title

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,4 +1,5 @@
 import React, { FC } from 'react';
+import { Platform } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
@@ -17,9 +18,17 @@ const RoutesContainer: FC<Record<string, unknown>> = (): JSX.Element => (
     <Stack.Navigator
       initialRouteName="Login"
       headerMode="screen"
-      screenOptions={({ route }): Record<string, unknown> => ({
-        headerBackTitle: route.name === 'SignUp' ? 'Login' : ''
-      })}
+      screenOptions={({ route }): Record<string, unknown> => {
+        const options = {
+          headerBackTitle: route.name === 'SignUp' ? 'Login' : ''
+        };
+
+        if (Platform.OS == 'android') {
+          Object.assign(options, { headerBackTitleVisible: true });
+        }
+
+        return options;
+      }}
     >
       <Stack.Screen
         name="Login"
