0a031d2a4ee3b58c2a143952705affa59171b5da
Andrew Hummel <ahummel25@gmail.com>
2020-12-01 20:47:20 -0600

    Fix status bar for android

--- src/pages/signup.tsx
+++ src/pages/signup.tsx
@@ -3,7 +3,7 @@ import { Text, View } from 'react-native';
 
 const SignUp: FC<Record<string, unknown>> = (): JSX.Element => (
   <View>
-    <Text>Sign Up!</Text>
+    <Text></Text>
   </View>
 );
 
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -13,35 +13,51 @@ export type RootStackList = {
 
 const Stack = createStackNavigator<RootStackList>();
 
-const RoutesContainer: FC<Record<string, unknown>> = (): JSX.Element => (
-  <NavigationContainer>
-    <Stack.Navigator
-      initialRouteName="Login"
-      headerMode="screen"
-      screenOptions={({ route }): Record<string, unknown> => {
-        const options = {
-          headerBackTitle: route.name === 'SignUp' ? 'Login' : ''
-        };
+const RoutesContainer: FC<Record<string, unknown>> = (): JSX.Element => {
+  const loginOptions = {
+    headerTitle: ''
+  };
 
-        if (Platform.OS == 'android') {
-          Object.assign(options, { headerBackTitleVisible: true });
-        }
+  const signUpOptions = {
+    headerTitle: ''
+  };
 
-        return options;
-      }}
-    >
-      <Stack.Screen
-        name="Login"
-        component={Login}
-        options={{ headerTitle: '' }}
-      />
-      <Stack.Screen
-        name="SignUp"
-        component={SignUp}
-        options={{ headerTitle: '' }}
-      />
-    </Stack.Navigator>
-  </NavigationContainer>
-);
+  if (Platform.OS === 'android') {
+    Object.assign(loginOptions, { headerStyle: { height: 80 } });
+    Object.assign(signUpOptions, {
+      headerBackTitleStyle: { bottom: 2 },
+      headerStyle: { height: 100 }
+    });
+  }
+
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName="Login"
+        headerMode="screen"
+        screenOptions={({ route }): Record<string, unknown> => {
+          const options = {};
+
+          if (Platform.OS === 'android') {
+            Object.assign(options, { headerBackTitleVisible: true });
+          } else {
+            Object.assign(options, {
+              headerBackTitle: route.name === 'SignUp' ? 'Login' : ''
+            });
+          }
+
+          return options;
+        }}
+      >
+        <Stack.Screen name="Login" component={Login} options={loginOptions} />
+        <Stack.Screen
+          name="SignUp"
+          component={SignUp}
+          options={signUpOptions}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
 
 export default RoutesContainer;
