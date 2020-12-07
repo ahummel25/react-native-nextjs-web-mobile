e0d1c563b1c7bd764cf0dfa0e9bed24d2077ee95
Andrew Hummel <ahummel25@gmail.com>
2020-12-07 09:06:33 -0600

    Fix sign-up route

---
+++ src/pages/sign-up.tsx
@@ -0,0 +1,10 @@
+import React, { FC } from 'react';
+import { Text, View } from 'react-native';
+
+const SignUp: FC<Record<string, unknown>> = (): JSX.Element => (
+  <View>
+    <Text>!!!</Text>
+  </View>
+);
+
+export default SignUp;
