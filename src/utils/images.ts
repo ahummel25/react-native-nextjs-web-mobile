52f60b35555a3bf7e00c6306b04038dbdd02164a
Andrew Hummel <ahummel25@gmail.com>
2021-01-02 09:54:04 -0600

    Use icon param name in png file path

--- src/utils/images.ts
+++ src/utils/images.ts
@@ -1,9 +1,9 @@
 import { ImageSourcePropType } from 'react-native';
 
-export const getImageIcon = (name: string): ImageSourcePropType => {
-  switch (name) {
+export const getImageIcon = (iconName: string): ImageSourcePropType => {
+  switch (iconName) {
     case 'back':
-      return require('../../assets/images/back.png');
+      return require(`../../assets/images/${iconName}.png`);
     default:
       return require('../../assets/images/back.png');
   }
