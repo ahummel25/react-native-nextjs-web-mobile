b5e78b2476686f7fe2ff3cc87ef767703bb49321
Andrew Hummel <ahummel25@gmail.com>
2021-01-02 22:34:24 -0600

    Cannot do dynamic require

--- src/utils/images.ts
+++ src/utils/images.ts
@@ -3,7 +3,7 @@ import { ImageSourcePropType } from 'react-native';
 export const getImageIcon = (iconName: string): ImageSourcePropType => {
   switch (iconName) {
     case 'back':
-      return require(`../../assets/images/${iconName}.png`);
+      return require('../../assets/images/back.png');
     default:
       return require('../../assets/images/back.png');
   }
